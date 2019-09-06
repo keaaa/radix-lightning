c6705d0149a4b79fb4ad631a5a470b57ca6d39d2
Kjell Erik Reed Anda <keaaa@statoil.com>
2019-09-06 19:45:45 +0200

    first commit

---
+++ www/src/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Echo from './components/Echo'
+import Header from './components/Header'
+import EchoService from './services/Echo'  
+import './App.css';
+
+class App extends React.Component {
+  constructor() {
+    super()
+
+    this.refreshInterval = 1000;
+    this.mockData = false;
+    
+    this.state ={
+      echoResult: {},
+      nrRefresh: 0
+    }
+  }
+
+  componentDidMount() {
+    setInterval(this.loadData, this.refreshInterval) 
+  }
+
+  loadData = () => {
+    const echoService = EchoService(this.mockData)
+    const nrRefresh = this.state.nrRefresh + 1
+
+    echoService.fetch('/api/echo').then(data => this.setState({ echoResult: data, nrRefresh: nrRefresh}))
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Echo result={this.state.echoResult} refreshInterval={this.refreshInterval} nrRefresh={this.state.nrRefresh} />
+      </div>
+    );
+  }
+}
+
+export default App;
