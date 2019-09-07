de94690c33ebb691820b15204156ff135a8d6f72
Kjell Erik Reed Anda <keaaa@statoil.com>
2019-09-07 20:58:46 +0200

    3

--- www/src/components/Header.js
+++ www/src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
   return <header className="App-header">
     <img src={logo} className="App-logo" alt="logo" />
     <p>
-      Welcome to Radix workshop
+      Welcome to Radix lightning talk
     </p>
     <a
       className="App-link"
