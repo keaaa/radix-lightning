cd0c376af1a5b620ac9264a5a6cf5a93c3437ccf
Kjell Erik Reed Anda <keaaa@statoil.com>
2019-09-09 10:56:37 +0200

    1

--- www/src/components/Todo.js
+++ www/src/components/Todo.js
@@ -5,7 +5,15 @@ const Todo = () => {
     return <div>
         TODO List:
         <br />
-        - deploy UI
+        - register app in radix
+        <br />
+        - deploy UI with mock data
+        <br />
+        - verified app
+        <br />
+        - connect API to UI and deploy
+        <br />
+        - promoted to production
     </div>
 }
 
